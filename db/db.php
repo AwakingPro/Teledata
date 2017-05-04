3245125f85790064a7b78433f0b415fc900fa7dd
Oswaldo Rodriguez <bfsraptor@gmail.com>
2017-05-04 13:23:37 -0430

    Colocando archivos db como estaban

--- db/db.php
+++ db/db.php
@@ -1,8 +1,8 @@
 <?php
-	// $host_name = '192.168.1.8';
-	// $pass_word = 's9q7l5.,777';
-	$host_name = 'localhost';
-	$pass_word = '';
+	$host_name = '192.168.1.8';
+	$pass_word = 's9q7l5.,777';
+	// $host_name = 'localhost';
+	// $pass_word = '';
 	$user_name = 'root';
 	$database_name = 'teledata';
 	$conn = mysql_connect($host_name, $user_name, $pass_word) or die ('Error connecting to mysql');
