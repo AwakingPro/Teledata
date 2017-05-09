d047f2d91068fb9574e0d1d83987a84d07dffe68
arincon21 <arincon@outlook.com>
2017-05-08 21:51:36 -0400

    Depuracion de maqueta

--- tickets/index.php
+++ tickets/index.php
@@ -6,18 +6,8 @@
 		<title>Foco | Software de Estrategia</title>
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
 		<link href="../css/nifty.min.css" rel="stylesheet">
-		<link href="../premium/icon-sets/solid-icons/premium-solid-icons.min.css" rel="stylesheet">
-		<link href="../plugins/ionicons/css/ionicons.min.css" rel="stylesheet">
-		<link href="../plugins/themify-icons/themify-icons.min.css" rel="stylesheet">
 		<link href="../css/demo/nifty-demo-icons.min.css" rel="stylesheet">
 		<link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-		<link href="../plugins/animate-css/animate.min.css" rel="stylesheet">
-		<link href="../plugins/switchery/switchery.min.css" rel="stylesheet">
-		<link href="../plugins/morris-js/morris.min.css" rel="stylesheet">
-		<link href="../css/demo/nifty-demo.min.css" rel="stylesheet">
-		<link href="../plugins/pace/pace.min.css" rel="stylesheet">
-		<script src="../plugins/pace/pace.min.js"></script>
-		<link href="../plugins/bootstrap-select/bootstrap-select.min.css" rel="stylesheet">
 		<link href="../css/themes/type-a/theme-dark.min.css" rel="stylesheet">
 	</head>
 	<body>
@@ -198,48 +188,14 @@
 						</div>
 					</div>
 				</div>
-				<!--===================================================-->
-				<!--End page content-->
 			</div>
-			<!--===================================================-->
-			<!--END CONTENT CONTAINER-->
-			<!--MAIN NAVIGATION-->
-			<!--===================================================-->
 			<?php include("../layout/main-menu.php"); ?>
-			<!--===================================================-->
-			<!--END MAIN NAVIGATION-->
-			<!--ASIDE-->
-			<!--===================================================-->
-			<!--END ASIDE-->
 		</div>
-		<!-- FOOTER -->
-		<!--===================================================-->
 		<?php include("../layout/footer.php"); ?>
-		<!--===================================================-->
-		<!-- END FOOTER -->
-		<!-- SCROLL TOP BUTTON -->
-		<!--===================================================-->
-		<button id="scroll-top" class="btn"><i class="fa fa-chevron-up"></i></button>
-		<div class="modal"><!-- Place at bottom of page --></div>
-		<!--===================================================-->
 	</div>
-	<!--===================================================-->
-	<!-- END OF CONTAINER -->
-	<!--JAVASCRIPT-->
 	<script src="../js/jquery-2.2.1.min.js"></script>
-	<script src="../js/usuarios/usuarios.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
-	<script src="../plugins/fast-click/fastclick.min.js"></script>
 	<script src="../js/nifty.min.js"></script>
-	<script src="../plugins/morris-js/morris.min.js"></script>
-	<script src="../plugins/morris-js/raphael-js/raphael.min.js"></script>
-	<script src="../plugins/sparkline/jquery.sparkline.min.js"></script>
-	<script src="../plugins/skycons/skycons.min.js"></script>
-	<script src="../plugins/switchery/switchery.min.js"></script>
-	<script src="../plugins/bootstrap-select/bootstrap-select.min.js"></script>
-	<script src="../js/demo/nifty-demo.min.js"></script>
 	<script src="../plugins/bootbox/bootbox.min.js"></script>
-	<script src="../js/demo/ui-alerts.js"></script>
-	<script src="../js/global/funciones-global.js"></script>
 </body>
 </html>
