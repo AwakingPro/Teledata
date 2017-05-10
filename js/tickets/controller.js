84d4c7c0924d22c3d8387d7407ba36eb15a8567c
arincon21 <arincon@outlook.com>
2017-05-09 22:13:47 -0400

    cambios en controller de mod tickets

--- js/tickets/controller.js
+++ js/tickets/controller.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
 	$('.guardarTicket').click(function() {
 		$.postFormValues('../ajax/tickets/insertData.php', function(data){
 			if (Number(data) > 0){
+				$('.listaAbiertos').load('../ajax/tickets/listAbiertos.php',function(){
+					$('.listaAbiertos .tabeData').dataTable();
+				});
 				bootbox.alert('<h3 class="text-center">El ticket se registro con exito.</h3>');
 			}else{
 				bootbox.alert('<h3 class="text-center">Se produjo un error al guardar el ticket.</h3>');
