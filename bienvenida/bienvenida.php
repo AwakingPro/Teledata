f8fb12ef2196fe69d40d521396b24e226e5540c6
arincon21 <arincon@outlook.com>
2017-05-13 11:31:31 -0400

    Modificasion de pantalla de bienvenida

--- bienvenida/bienvenida.php
+++ bienvenida/bienvenida.php
@@ -1,154 +1,81 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foco | Software de Estrategia</title>
-    <!--STYLESHEET-->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/nifty.min.css" rel="stylesheet">
-    <link href="../premium/icon-sets/solid-icons/premium-solid-icons.min.css" rel="stylesheet">
-    <link href="../plugins/ionicons/css/ionicons.min.css" rel="stylesheet">
-    <link href="../plugins/themify-icons/themify-icons.min.css" rel="stylesheet">
-    <link href="../css/demo/nifty-demo-icons.min.css" rel="stylesheet">
-    <link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../plugins/animate-css/animate.min.css" rel="stylesheet">
-    <link href="../plugins/switchery/switchery.min.css" rel="stylesheet">
-    <link href="../plugins/morris-js/morris.min.css" rel="stylesheet">
-    <link href="../css/demo/nifty-demo.min.css" rel="stylesheet">
-    <link href="../plugins/pace/pace.min.css" rel="stylesheet">
-    <script src="../plugins/pace/pace.min.js"></script>
-    <link href="../plugins/bootstrap-select/bootstrap-select.min.css" rel="stylesheet">
-    <link href="../css/themes/type-a/theme-dark.min.css" rel="stylesheet">
-
-    <style type="text/css">
-
-#navbar .brand-title {
-    padding: 0 1.5em 0 5px;
-}
-
-    </style>
-</head>
-<body>
-  <div id="container" class="effect mainnav-lg aside-in">
-    <!--NAVBAR-->
-    <!--===================================================-->
-    <?php
-    include("../layout/header.php");
-    ?>
-    <!--===================================================-->
-    <!--END NAVBAR-->
-      <div class="boxed">
-        <!--CONTENT CONTAINER-->
-        <!--===================================================-->
-        <div id="content-container">
-          <!--Page Title-->
-          <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-          <div id="page-title">
-          <!--Searchbox-->
-          </div>
-          <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-          <!--End page title-->
-          <!--Breadcrumb-->
-          <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-          <!--<ol class="breadcrumb">
-            <li><a href="#">Inicio</a></li>
-            <li class="active">Bienvenido</li>
-          </ol>-->
-          <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-          <!--End breadcrumb-->
-          <!--Page content-->
-          <!--===================================================-->
-          <div id="page-content">
-            <div class="row">
-						  <div class="eq-height">
-  						  <div class="col-sm-12 eq-box-sm">
-                  <div class="panel">
-
-								<div class="panel-bg-cover">
-									<img class="img-responsive" src="../img/thumbs/img1.jpg" alt="Image">
-								</div>
-								<div class="panel-media">
-                  <img class="panel-media-img img-circle img-border-light" src="../img/av1.png" alt="Profile Picture">
-
-									<div class="row">
-										<div class="col-lg-7">
-											<h3 class="panel-media-heading"></h3>
-											<a href="#" class="btn-link"></a>
-											<p class="text-muted mar-btm"></p>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>ERP | Teledata</title>
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/nifty.min.css" rel="stylesheet">
+		<link href="../css/demo/nifty-demo-icons.min.css" rel="stylesheet">
+		<link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+		<link href="../css/themes/type-a/theme-dark.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="container" class="effect mainnav-lg">
+			<?php
+			include("../layout/header.php");
+			?>
+			<div class="boxed">
+				<div id="content-container">
+					<div id="page-title">
+						<h1>Bienvenidos a ERP Teledata</h1>
+					</div>
+					<div id="page-content">
+						<div class="row">
+							<div class="eq-height">
+								<div class="col-sm-12 eq-box-sm">
+									<div class="panel">
+										<div class="panel-bg-cover">
+											<img class="img-responsive" src="../img/thumbs/img1.jpg" alt="Image">
 										</div>
-										<div class="col-lg-5 text-lg-right">
-
+										<div class="panel-media">
+											<img class="panel-media-img img-circle img-border-light" src="../img/av1.png" alt="Profile Picture">
+											<div class="row">
+												<div class="col-lg-7">
+													<h3 class="panel-media-heading"></h3>
+													<a href="#" class="btn-link"></a>
+													<p class="text-muted mar-btm"></p>
+												</div>
+												<div class="col-lg-5 text-lg-right">
+												</div>
+											</div>
+										</div>
+										<div class="panel-body">
+											<h4>Teledata ERP</h4>
+											<br>
+											Para obtener ayuda descargue aquí el manual de usuario
+											<i class="pli-information icon-lg icon-fw"></i> Ayuda
 										</div>
 									</div>
 								</div>
-								<div class="panel-body">
-									<h4>Teledata ERP</h4>
-                  <br>
-                  Para obtener ayuda descargue aquí el manual de usuario
-
-                  <i class="pli-information icon-lg icon-fw"></i> Ayuda
-
-
-								</div>
 							</div>
-
-                      </div>
-
-
-
-                      <!--===================================================-->
-      								<!--End Panel model-->
-                </div>
-						  </div>
-					  </div>
-          </div>
-          <!--===================================================-->
-          <!--End page content-->
-        </div>
-        <!--===================================================-->
-        <!--END CONTENT CONTAINER-->
-        <!--MAIN NAVIGATION-->
-        <!--===================================================-->
-        <?php include("../layout/main-menu.php"); ?>
-        <!--===================================================-->
-        <!--END MAIN NAVIGATION-->
-        <!--ASIDE-->
-
-
-
-
-        <!--===================================================-->
-        <!--END ASIDE-->
-      </div>
-        <!-- FOOTER -->
-        <!--===================================================-->
-        <?php include("../layout/footer.php"); ?>
-        <!--===================================================-->
-        <!-- END FOOTER -->
-        <!-- SCROLL TOP BUTTON -->
-        <!--===================================================-->
-        <button id="scroll-top" class="btn"><i class="fa fa-chevron-up"></i></button>
-        <div class="modal"><!-- Place at bottom of page --></div>
-        <!--===================================================-->
-    </div>
-    <!--===================================================-->
-    <!-- END OF CONTAINER -->
-    <!--JAVASCRIPT-->
-    <script src="../js/jquery-2.2.1.min.js"></script>
-    <script src="../js/usuarios/usuarios.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../plugins/fast-click/fastclick.min.js"></script>
-    <script src="../js/nifty.min.js"></script>
-	  <script src="../plugins/morris-js/morris.min.js"></script>
-    <script src="../plugins/morris-js/raphael-js/raphael.min.js"></script>
-    <script src="../plugins/sparkline/jquery.sparkline.min.js"></script>
-    <script src="../plugins/skycons/skycons.min.js"></script>
-    <script src="../plugins/switchery/switchery.min.js"></script>
-    <script src="../plugins/bootstrap-select/bootstrap-select.min.js"></script>
-    <script src="../js/demo/nifty-demo.min.js"></script>
-    <script src="../plugins/bootbox/bootbox.min.js"></script>
-    <script src="../js/demo/ui-alerts.js"></script>
-    <script src="../js/global/funciones-global.js"></script>
+						</div>
+					</div>
+				</div>
+			</div>
+			<nav id='mainnav-container'>
+				<div id='mainnav'>
+					<div id='mainnav-shortcut'>
+						<ul class='list-unstyled'>
+							<li class='col-xs-4' data-content='Page Alerts'></li>
+						</ul>
+					</div>
+					<div id='mainnav-menu-wrap'>
+						<div class='nano'>
+							<div class='nano-content'>
+								<ul id='mainnav-menu' class='list-group'>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<script src="../js/jquery-2.2.1.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/nifty.min.js"></script>
+	<script src="../js/demo/nifty-demo.min.js"></script>
+	<script src="../js/methods_global/methods.js"></script>
 </body>
 </html>
