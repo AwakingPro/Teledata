4b06f59e7c96a43f1cc08338d0aeb4b107b22a9c
arincon21 <arincon@outlook.com>
2017-05-22 16:37:33 -0400

    .sql del munu con tikect agregado

---
+++ ajax/menu/menu.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : teledata
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2017-05-22 16:37:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(10) NOT NULL,
+  `descripcion` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `enlace` varchar(100) NOT NULL,
+  `permisos` varchar(20) NOT NULL,
+  `icono` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_menu`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
