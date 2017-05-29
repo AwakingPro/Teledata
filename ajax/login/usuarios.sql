e9ef8dd08e5027d890e7f7fa6b10e59733fbcfcf
arincon21 <arincon@outlook.com>
2017-05-28 21:28:45 -0400

    .sql de usuario actualizado

--- ajax/login/usuarios.sql
+++ ajax/login/usuarios.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-05-26 18:09:43
+Date: 2017-05-28 21:27:53
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,7 +30,7 @@ CREATE TABLE `usuarios` (
   `sexo` varchar(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=93 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=96 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of usuarios
@@ -38,3 +38,6 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` VALUES ('1', 'lponcez', 'Luis Ponce', '$2y$10$8oWS5shtgP4Y8XP/R61P1O0h7WZAZM/tK/1Xs62hOPoWMksdtgPba', '1', 'CTO', 'luis@awaking.cl', 'M');
 INSERT INTO `usuarios` VALUES ('89', 'oswaldo', 'oswaldo', '$2y$10$./MECKBhiBxbl0MJp893COX44cFa/HDKKeJ0l1yOmNzJxjmrDNDEG', '1', 'Desarrollo', '', 'M');
 INSERT INTO `usuarios` VALUES ('90', 'arincon', 'Alvaro Rincon', '$2y$10$/.Lqnt3GG4n40.S.U9gltOKPNNuy/FN92WWR7XnisZmIK/Amx29.W', '1', 'Desarrollador', '', '');
+INSERT INTO `usuarios` VALUES ('93', 'administrador', 'Administrador', '$2y$10$/.Lqnt3GG4n40.S.U9gltOKPNNuy/FN92WWR7XnisZmIK/Amx29.W', '1', 'Administrador', '', '');
+INSERT INTO `usuarios` VALUES ('94', 'soporte', 'Soporte', '$2y$10$/.Lqnt3GG4n40.S.U9gltOKPNNuy/FN92WWR7XnisZmIK/Amx29.W', '2', 'Soporte', '', '');
+INSERT INTO `usuarios` VALUES ('95', 'terreno', 'Terreno', '$2y$10$/.Lqnt3GG4n40.S.U9gltOKPNNuy/FN92WWR7XnisZmIK/Amx29.W', '3', 'Terreno', '', '');
