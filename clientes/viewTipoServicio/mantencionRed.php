100b593f7abbf6725a8df02435b174fca59cd731
Alvaro Rincon <arincon@outlook.com>
2017-06-13 14:56:46 -0400

    cambios

---
+++ clientes/viewTipoServicio/mantencionRed.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class="col-md-12">
+		<h3>Mantencion de Red</h3>
+	</div>
+	<div class="col-md-12 form-group" rows="5">
+		<label>Descripcion</label>
+		<textarea name="descripcion" class="form-control"></textarea>
+	</div>
+	<div class="col-md-12 form-group" rows="5">
+		<label>Comentario</label>
+		<textarea name="comentario" class="form-control"></textarea>
+	</div>
+</div>
---
+++ clientes/viewTipoServicio/mensualidadIPFija.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class="col-md-12">
+		<h3>Registro de Ip Fija</h3>
+	</div>
+	<div class="col-md-12 form-group" rows="5">
+		<label>Direccion IP Fija</label>
+		<textarea name="ip" class="form-control"></textarea>
+	</div>
+	<div class="col-md-12 form-group" rows="5">
+		<label>Descripcion</label>
+		<textarea name="descripcion" class="form-control"></textarea>
+	</div>
+</div>
