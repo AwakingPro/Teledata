7c20d67bce43589ce17dcccb829ddcc8c7cc736a
arincon21 <arincon@outlook.com>
2017-07-16 22:30:57 -0400

    modificasion de en agrupar facturas

--- ajax/reportesFacturas/reporteFacturasClientes.php
+++ ajax/reportesFacturas/reporteFacturasClientes.php
@@ -1,18 +1,18 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
 	$query = "SELECT
-		servicios.Codigo,
-		personaempresa.rut as 'Rut',
-		personaempresa.nombre as 'Nombre',
-		mantenedor_servicios.servicio as 'Servicios',
-		servicios.Grupo,
-		sum(servicios.Valor) as 'Monto'
+	servicios.Codigo,
+	personaempresa.rut as 'Rut',
+	personaempresa.nombre as 'Nombre',
+	mantenedor_servicios.servicio as 'Servicio',
+	CONCAT('Grupo ',servicios.Grupo) as 'Grupo',
+	Sum(servicios.Valor) as 'Valor'
 	FROM
 		servicios
 		INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut
 		INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio
 	GROUP BY
-	personaempresa.rut";
+	servicios.Grupo";
 	$run = new Method;
 	$lista = $run->listViewFacturasClientes($query);
 	echo $lista;
