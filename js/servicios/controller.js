eca42063ef296e36bf5b892c4008499e295a2a98
arincon21 <arincon@outlook.com>
2017-07-16 23:29:58 -0400

    cambio en campo numerico

--- js/servicios/controller.js
+++ js/servicios/controller.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('[name="Valor"]').number( true, 2 );
+	$('[name="Valor"]').number( true, 0,',','.');
 	$('select[name="Rut"]').load('../ajax/servicios/selectClientes.php',function(){
 		$('select[name="Rut"]').selectpicker();
 	});
