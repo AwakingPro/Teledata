3d1e4cf38107ef0f2152b9413fa6681f8284e859
arincon21 <arincon@outlook.com>
2017-07-16 21:54:10 -0400

    cambios en grupos

--- ajax/reportesFacturas/reporteFacturasClientes.php
+++ ajax/reportesFacturas/reporteFacturasClientes.php
@@ -1,15 +1,17 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
 	$query = "SELECT
-	servicios.Codigo,
-	personaempresa.rut,
-	personaempresa.nombre,
-	mantenedor_servicios.servicio,
-	servicios.Valor
+		servicios.Codigo,
+		personaempresa.rut as 'Rut',
+		personaempresa.nombre as 'Nombre',
+		mantenedor_servicios.servicio as 'Servicios',
+		sum(servicios.Valor) as 'Monto'
 	FROM
-	servicios
-	INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut
-	INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio";
+		servicios
+		INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut
+		INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio
+	GROUP BY
+	personaempresa.rut";
 	$run = new Method;
 	$lista = $run->listViewFacturasClientes($query);
 	echo $lista;
--- ajax/tablas sql/menu.sql
+++ ajax/tablas sql/menu.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Mi Computadora
+Source Server         : localhost
 Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : teledata
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-06-15 10:20:00
+Date: 2017-07-16 20:41:53
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,11 +32,11 @@ CREATE TABLE `menu` (
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES ('1', 'Clientes', 'Clientes', '#', '1', 'fa fa-user');
-INSERT INTO `menu` VALUES ('3', 'Ventas', 'Costos', '../costos/Costo.php', '1', 'fa fa-dollar');
-INSERT INTO `menu` VALUES ('4', 'com', 'Compras & Ingreso', '../compras_ingresos/Ingreso.php', '1', 'fa fa-shopping-cart');
-INSERT INTO `menu` VALUES ('7', 'Inventario', 'Inventario', '#', '1', 'fa fa-dropbox');
-INSERT INTO `menu` VALUES ('8', 'Reportes', 'Reportes', '#', '1', 'fa fa-bar-chart');
+INSERT INTO `menu` VALUES ('1', 'Clientes', 'Clientes', '#', '1,2.3', 'fa fa-user');
+INSERT INTO `menu` VALUES ('3', 'Ventas', 'Costos', '../costos/Costo.php', '1,2,3', 'fa fa-dollar');
+INSERT INTO `menu` VALUES ('4', 'com', 'Compras & Ingreso', '../compras_ingresos/Ingreso.php', '1,2,3', 'fa fa-shopping-cart');
+INSERT INTO `menu` VALUES ('7', 'Inventario', 'Inventario', '#', '1,2,3', 'fa fa-dropbox');
+INSERT INTO `menu` VALUES ('8', 'Reportes', 'Reportes', '#', '1,2,3', 'fa fa-bar-chart');
 INSERT INTO `menu` VALUES ('9', 'Configurac', 'Configuracion', '#', '1', 'fa fa-cog');
-INSERT INTO `menu` VALUES ('10', 'Tickets', 'Tickets', '../Tickets', '1', 'fa fa-ticket');
-INSERT INTO `menu` VALUES ('14', 'RadioPlan', 'Radio Planning', '../radio/Radio.php', '1', 'fa fa-map-marker');
+INSERT INTO `menu` VALUES ('10', 'Tickets', 'Tickets', '../Tickets', '1,2,3', 'fa fa-ticket');
+INSERT INTO `menu` VALUES ('14', 'RadioPlan', 'Radio Planning', '../radio/Radio.php', '1,2,3', 'fa fa-map-marker');
