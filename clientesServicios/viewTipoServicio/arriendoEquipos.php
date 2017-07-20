2556245df99a86d38bdecb045126c2de428d6313
Alvaro Rincon <arincon@outlook.com>
2017-07-19 22:01:54 -0400

    cambio

--- clientesServicios/viewTipoServicio/arriendoEquipos.php
+++ clientesServicios/viewTipoServicio/arriendoEquipos.php
@@ -2,21 +2,54 @@
 	<div class="col-md-12">
 		<h3>Arriendo de equipos</h3>
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Tipo equipo</label>
-		<input name="tipoEquipo" class="form-control">
-		<input name="idServicio" type="hidden">
+	<div class="col-md-12">
+		<div class="form-group">
+			<label class="control-label">Seleccione Origen</label>
+			<select class="form-control" name="origen_tipo" data-live-search="true">
+				<option value="">Seleccione Opción</option>
+				<option value="1">Bodega</option>
+				<option value="2">Cliente</option>
+			</select>
+		</div>
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Modelo</label>
-		<input name="modelo" class="form-control">
+
+	<div class="col-md-12 origen" style="display:none">
+		<div class="form-group">
+			<label class="control-label">Seleccione <span id="span_origen">Bodega</span></label>
+			<select class="form-control" name="origen_id" data-live-search="true">
+				<option value="">Seleccione Opción</option>
+			</select>
+		</div>
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Mac/SN</label>
-		<input name="mac" class="form-control">
+
+	<div class="col-md-12">
+		<div class="form-group">
+			<label class="control-label">Activo a Transferir</label>
+			<select class="form-control" name="producto_id" data-live-search="true">
+				<option value="">Seleccione Opción</option>
+			</select>
+		</div>
 	</div>
-	<div class="col-md-12 form-group" rows="5">
-		<label>Descripcion</label>
-		<textarea name="descripcion" class="form-control"></textarea>
+
+	<div class="col-md-12">
+		<div class="form-group">
+			<label class="control-label">Seleccione Destino</label>
+			<select class="form-control" name="destino_tipo" data-live-search="true">
+				<option value="">Seleccione Opción</option>
+				<option value="1">Bodega</option>
+				<option value="2">Cliente</option>
+			</select>
+		</div>
 	</div>
+
+	<div class="col-md-12">
+		<div class="form-group">
+			<label class="control-label">Seleccione <span id="span_destino">Bodega</span></label>
+			<select class="form-control" name="destino_id" data-live-search="true">
+				<option value="">Seleccione Opción</option>
+			</select>
+		</div>
+	</div>
+
+
 </div>
