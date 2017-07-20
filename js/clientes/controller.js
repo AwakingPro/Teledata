353113afd0a8decf907e5f54f2980b12ca8704ad
Alvaro Rincon <arincon@outlook.com>
2017-07-20 01:31:54 -0400

    Migrar funciones de egresos

--- js/clientes/controller.js
+++ js/clientes/controller.js
@@ -376,7 +376,64 @@ $(document).ready(function() {
 
     });
 
+$(document).on('change', '#destino_tipo', function () {
 
+        $('#destino_id').empty();
+        $('#destino_id').append(new Option('Seleccione Opción',''));
+
+        if($(this).val()){
+
+            $('.destino').show();
+
+            if($(this).val() == 1){
+
+                $('#span_destino').text('Bodega');
+
+                $.ajax({
+                    type: "POST",
+                    url: "../includes/inventario/egresos/getBodega.php",
+                    success: function(response){
+
+                        $.each(response.array, function( index, array ) {
+                            $('#destino_id').append('<option value="'+array.id+'" data-content="'+array.nombre+'"></option>');
+                        });
+
+                        $('#destino_id').selectpicker('refresh');
+                    }
+                });
+            }else{
+                $('#span_destino').text('Cliente');
+                $('#destino_id').selectpicker('refresh');
+            }
+        }else{
+            $('.destino').hide();
+        }
+    });
+
+    $(document).on('change', '#origen_id', function () {
+
+        $('#producto_id').empty();
+        $('#producto_id').append(new Option('Seleccione Opción',''));
+
+        origen_tipo = $('#origen_tipo').val();
+        origen_id = $('#origen_id').val();
+
+        $.ajax({
+            type: "POST",
+            url: "../includes/inventario/egresos/getProducto.php",
+            data:"&origen_tipo="+origen_tipo+"&origen_id="+origen_id,
+            success: function(response){
+
+                $.each(response.array, function( index, array ) {
+                    $('#producto_id').append('<option value="'+array.id+'" data-content="'+array.tipo + ' ' + array.marca + ' ' + array.modelo+ ' - ' + array.numero_serie+'"></option>');
+                });
+
+                $('.selectpicker').selectpicker('render');
+                $('.selectpicker').selectpicker('refresh');
+            }
+        });
+
+    });
 
 
 });
