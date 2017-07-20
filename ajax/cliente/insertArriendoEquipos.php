655cbe511cfeb505dbf273955c9fd0c622c13f86
Alvaro Rincon <arincon@outlook.com>
2017-07-20 01:45:00 -0400

    cambios en Arriendo de equipos

--- ajax/cliente/insertArriendoEquipos.php
+++ ajax/cliente/insertArriendoEquipos.php
@@ -1,7 +1,8 @@
 <?php
-	require_once('../../class/methods_global/methods.php');
-	$query = "INSERT INTO arriendo_equipos_datos (TipoEquipo, Modelo, MacSN, Descripcion,IdServivio) VALUES ('".$_POST['tipoEquipo']."', '".$_POST['modelo']."', '".$_POST['mac']."', '".$_POST['descripcion']."', '".$_POST['idServicio']."')";
-	$run = new Method;
-	$data = $run->insert($query);
-	echo $data;
- ?>
+
+	include("../../class/inventario/egresos/EgresoClass.php");
+
+	$Egreso = new Egreso();
+	$Egreso->storeMovimiento($_POST['producto_id'],$_POST['destino_tipo'],$_POST['destino_id']);
+
+?>
