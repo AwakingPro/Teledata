8eb3f163614b8da6b7ccfd3932d53ae996722cd1
arincon21 <arincon@outlook.com>
2017-08-01 18:30:01 -0400

    cambios en insert nuevo usuario

--- ajax/registroUsuario/insertUser.php
+++ ajax/registroUsuario/insertUser.php
@@ -1,10 +1,10 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
 	$clave = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-	$query = "INSERT INTO usuarios (usuario, nombre, clave, nivel, cargo, email) VALUES ('".$_POST['usuario']."', '".$_POST['nombre']."', '".$clave."', '".$_POST['previlegios']."', '".$_POST['cargo']."', '".$_POST['correo']."')";
+	$query = "INSERT INTO usuarios (usuario, nombre, clave, nivel, cargo, email,sexo) VALUES ('".$_POST['usuario']."', '".$_POST['nombre']."', '".$clave."', '".$_POST['previlegios']."', '".$_POST['cargo']."', '".$_POST['correo']."','')";
 	$run = new Method;
 	$data = $run->insert($query);
-	echo $query;
+	echo $data;
  ?>
 
 
