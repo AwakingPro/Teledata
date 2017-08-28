a9e3ac3efc484d47bbef104332245adc084cdb5d
Alvaro Rincon <arincon@outlook.com>
2017-08-28 13:39:03 -0400

    cambios 12

--- ajax/reportesFacturas/reporteFacturasClientes.php
+++ ajax/reportesFacturas/reporteFacturasClientes.php
@@ -1,16 +1,6 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
-	$query = "SELECT
-	servicios.Codigo,
-	personaempresa.rut as 'Rut',
-	personaempresa.nombre as 'Nombre',
-	mantenedor_servicios.servicio as 'Servicio',
-	CONCAT('Grupo ',servicios.Grupo) as 'Grupo',
-	servicios.Valor
-	FROM
-		servicios
-		INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut
-		INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio";
+	$query = "SELECT servicios.Codigo, personaempresa.rut as 'Rut', personaempresa.nombre as 'Nombre', mantenedor_servicios.servicio as 'Servicio', CONCAT('Grupo ',servicios.Grupo) as 'Grupo', servicios.Valor FROM servicios INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio";
 	$run = new Method;
 	$lista = $run->listViewFacturasClientes($query);
 	echo $lista;
