4a3ccb727e63ddd555016c372ebb2c52dd8b6eee
Alvaro Rincon <arincon@outlook.com>
2017-08-28 13:33:38 -0400

    cambio

--- ajax/reportesFacturas/reporteFacturasClientes.php
+++ ajax/reportesFacturas/reporteFacturasClientes.php
@@ -6,13 +6,11 @@
 	personaempresa.nombre as 'Nombre',
 	mantenedor_servicios.servicio as 'Servicio',
 	CONCAT('Grupo ',servicios.Grupo) as 'Grupo',
-	Sum(servicios.Valor) as 'Valor'
+	servicios.Valor
 	FROM
 		servicios
 		INNER JOIN personaempresa ON servicios.Rut = personaempresa.rut
-		INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio
-	GROUP BY
-	servicios.Grupo";
+		INNER JOIN mantenedor_servicios ON servicios.IdServicio = mantenedor_servicios.IdServicio";
 	$run = new Method;
 	$lista = $run->listViewFacturasClientes($query);
 	echo $lista;
