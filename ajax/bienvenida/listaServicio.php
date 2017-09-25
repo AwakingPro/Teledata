bb64369d43f89c76d36062f01bb6096fe5f5c43e
arincon21 <arincon@outlook.com>
2017-09-24 20:23:09 -0400

    lista de servicios

---
+++ ajax/bienvenida/listaServicio.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('../../class/methods_global/methods.php');
+	session_start();
+	$query = 'SELECT
+		servicios.Id,
+		servicios.Codigo,
+		servicios.TiepoFacturacion as Tiempo_Facturacion,
+		mantenedor_tipo_factura.descripcion as Descripcion,
+		servicios.Grupo
+		FROM
+		servicios
+		INNER JOIN mantenedor_tipo_factura ON servicios.TipoFactura = mantenedor_tipo_factura.codigo
+	WHERE
+		servicios.IdUsuarioSession ='.$_SESSION['idUsuario'];
+	$run = new Method;
+	$lista = $run->listViewServicios($query);
+	echo $lista;
+
+ ?>
--- ajax/servicios/insertServicio.php
+++ ajax/servicios/insertServicio.php
@@ -1,23 +1,24 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
+	session_start();
 	$run = new Method;
 	$dv = $run->select("SELECT dv FROM PersonaEmpresa WHERE rut = ".$_POST['Rut']);
 	$dv = $dv[0][0];
 	$Contar = count($run->select("SELECT Rut FROM servicios WHERE Rut = ". $_POST['Rut'] ."  AND IdServicio = ".$_POST['TipoServicio']));
-    $Contar = $Contar+1;
-    if($Contar<10){
-    	$ContarFinal = "0".$Contar;
-    }else{
-    	$ContarFinal = $Contar;
-    }
+	$Contar = $Contar+1;
+	if($Contar<10){
+		$ContarFinal = "0".$Contar;
+	}else{
+		$ContarFinal = $Contar;
+	}
 
-    $Descuento = $_POST['Descuento'];
-    if(!$Descuento){
-    	$Descuento = 0;
-    }
+	$Descuento = $_POST['Descuento'];
+	if(!$Descuento){
+		$Descuento = 0;
+	}
 
 	$Codigo = $_POST['Rut']."-".$dv.$_POST['TipoFactura'].$ContarFinal;
-	$query = "INSERT INTO servicios (Rut, Grupo, TipoFactura, Valor, Descuento, IdServicio, TiepoFacturacion, Codigo, Descripcion,TipoMoneda) VALUES ('".$_POST['Rut']."', '".$_POST['Grupo']."','".$_POST['TipoFactura']."' , '".$_POST['Valor']."','$Descuento' ,'".$_POST['TipoServicio']."' ,'".$_POST['TiepoFacturacion']."' ,'".$Codigo."' ,'".$_POST['Descripcion']."', '".$_POST['tipoMoneda']."' )";
+	$query = "INSERT INTO servicios (Rut, Grupo, TipoFactura, Valor, Descuento, IdServicio, TiepoFacturacion, Codigo, Descripcion,TipoMoneda, IdUsuarioSession) VALUES ('".$_POST['Rut']."', '".$_POST['Grupo']."','".$_POST['TipoFactura']."' , '".$_POST['Valor']."','$Descuento' ,'".$_POST['TipoServicio']."' ,'".$_POST['TiepoFacturacion']."' ,'".$Codigo."' ,'".$_POST['Descripcion']."', '".$_POST['tipoMoneda']."', '".$_SESSION['idUsuario']."' )";
 	$data = $run->insert($query);
 	echo $data
  ?>
