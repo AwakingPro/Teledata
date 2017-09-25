1d0344e61495ec60a6c937de330b112a0ed99075
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-09-24 22:32:14 -0400

    Arreglando ingreso de servicios

--- ajax/servicios/insertServicio.php
+++ ajax/servicios/insertServicio.php
@@ -17,8 +17,22 @@
 		$Descuento = 0;
 	}
 
+    $Rut = $_POST['Rut'];
+    $Grupo = $_POST['Grupo'];
+    $TipoFactura = $_POST['TipoFactura'];
+    $Valor = $_POST['Valor'];
+    $TipoServicio = $_POST['TipoServicio'];
+    $TiepoFacturacion = $_POST['TiepoFacturacion'];
+    $Descripcion = $_POST['Descripcion'];
+    $tipoMoneda = $_POST['tipoMoneda'];
+    $idUsuario = $_SESSION['idUsuario'];
+    $Alias = $_POST['Alias'];
+
 	$Codigo = $_POST['Rut']."-".$dv.$_POST['TipoFactura'].$ContarFinal;
-	$query = "INSERT INTO servicios (Rut, Grupo, TipoFactura, Valor, Descuento, IdServicio, TiepoFacturacion, Codigo, Descripcion,TipoMoneda, IdUsuarioSession, Alias, Estatus, FechaInstalacion, InstaladoPor, Comentario, UsuarioPppoe) VALUES ('".$_POST['Rut']."', '".$_POST['Grupo']."','".$_POST['TipoFactura']."' , '".$_POST['Valor']."','$Descuento' ,'".$_POST['TipoServicio']."' ,'".$_POST['TiepoFacturacion']."' ,'".$Codigo."' ,'".$_POST['Descripcion']."', '".$_POST['tipoMoneda']."', '".$_SESSION['idUsuario']."', '".$_POST['Alias']."','','','','','')";
+	$query = " INSERT INTO servicios 
+                (Rut, Grupo, TipoFactura, Valor, Descuento, IdServicio, TiepoFacturacion, Codigo, Descripcion, TipoMoneda, IdUsuarioSession, Alias, Estatus, FechaInstalacion, InstaladoPor, Comentario, UsuarioPppoe) 
+                VALUES  
+                ('$Rut', '$Grupo','$TipoFactura' , '$Valor','$Descuento' ,'$TipoServicio' ,'$TiepoFacturacion' ,'$Codigo' ,'$Descripcion', '$tipoMoneda', '$idUsuario', '$Alias','','','','','')";
 	$data = $run->insert($query);
 	echo $data
  ?>
