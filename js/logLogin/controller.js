0b134a5d49cd8ee36b3cca6e5f1c8829a3558e5e
Alvaro Rincon <arincon@outlook.com>
2017-09-27 13:31:25 -0400

    spinner load

--- js/logLogin/controller.js
+++ js/logLogin/controller.js
@@ -1,3 +1,4 @@
+$('.listaLog').html('<div style="text-align:center; font-size:15px;">Cargando Informacion...</div><div class="spinner loading"></div>');
 $(document).ready(function() {
 	$('.listaLog').load('../ajax/logLogin/listaLog.php',function(){
 		$('.listaLog > .tabeData').dataTable();
--- js/logQuery/controller.js
+++ js/logQuery/controller.js
@@ -1,3 +1,5 @@
+$('.listaLog').html('<div style="text-align:center; font-size:15px;">Cargando Informacion...</div><div class="spinner loading"></div>');
+
 $(document).ready(function() {
 	$('.listaLog').load('../ajax/logQuery/listaLog.php',function(){
 		$('.listaLog > .tabeData').dataTable();
