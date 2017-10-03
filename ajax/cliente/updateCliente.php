d312db8b64c558a51fb86802155b044e25fc6943
Oswaldo Rodriguez <bfsraptor@hotmail.com>
2017-10-03 18:50:07 -0400

    Agregado campos de comuna y ciudad en clientes

--- ajax/cliente/updateCliente.php
+++ ajax/cliente/updateCliente.php
@@ -1,6 +1,22 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
-	$query = "UPDATE personaempresa SET rut='".$_POST['Rut_update']."', dv='".$_POST['Dv_update']."', nombre='".$_POST['Nombre_update']."', giro='".$_POST['Giro_update']."', direccion='".$_POST['DireccionComercial_update']."', correo='".$_POST['Correo_update']."', contacto='".$_POST['Contacto_update']."', comentario='".$_POST['Comentario_update']."', telefono='".$_POST['Telefono_update']."', tipo_cliente='".$_POST['TipoCliente_update']."' WHERE id = ".$_POST['IdCliente'];
+
+	$Rut = isset($_POST['Rut_update']) ? trim($_POST['Rut_update']) : "";
+	$Dv = isset($_POST['Dv_update']) ? trim($_POST['Dv_update']) : "";
+	$Nombre = isset($_POST['Nombre_update']) ? trim($_POST['Nombre_update']) : "";
+	$Giro = isset($_POST['Giro_update']) ? trim($_POST['Giro_update']) : "";
+	$DireccionComercial = isset($_POST['DireccionComercial_update']) ? trim($_POST['DireccionComercial_update']) : "";
+	$Correo = isset($_POST['Correo_update']) ? trim($_POST['Correo_update']) : "";
+	$Contacto = isset($_POST['Contacto_update']) ? trim($_POST['Contacto_update']) : "";
+	$Comentario = isset($_POST['Comentario_update']) ? trim($_POST['Comentario_update']) : "";
+	$Telefono = isset($_POST['Telefono_update']) ? trim($_POST['Telefono_update']) : "";
+	$Alias = isset($_POST['Alias_update']) ? trim($_POST['Alias_update']) : "";
+	$TipoCliente = isset($_POST['TipoCliente_update']) ? trim($_POST['TipoCliente_update']) : "";
+	$Comuna = isset($_POST['Comuna_update']) ? trim($_POST['Comuna_update']) : "";
+	$Ciudad = isset($_POST['Ciudad_update']) ? trim($_POST['Ciudad_update']) : "";
+	$IdCliente = isset($_POST['IdCliente']) ? trim($_POST['IdCliente']) : "";
+
+	$query = "UPDATE personaempresa SET rut = '$Rut', dv = '$Dv', nombre = '$Nombre', giro = '$Giro', direccion = '$DireccionComercial', correo = '$Correo', contacto = '$Contacto', comentario = '$Comentario', telefono = '$Telefono', tipo_cliente = '$TipoCliente', comuna = '$Comuna', ciudad = '$Ciudad' WHERE id = '$IdCliente'";
 	$run = new Method;
 	$data = $run->update($query);
 	echo $data;
