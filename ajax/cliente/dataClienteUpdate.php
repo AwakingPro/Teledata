04cdce6c4c0993fb88d4908bc8628023a9050e2e
Alvaro Rincon <arincon@outlook.com>
2017-10-18 07:47:04 -0400

    selects listas

--- ajax/cliente/dataClienteUpdate.php
+++ ajax/cliente/dataClienteUpdate.php
@@ -1,11 +1,18 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
-	$query = 'SELECT *
-	FROM
-		personaempresa
-	WHERE
-		id ='.$_POST['id'];
 	$run = new Method;
+
+	$query = 'SELECT * FROM personaempresa WHERE id ='.$_POST['id'];
 	$data = $run->select($query);
+
+	$query = 'SELECT * FROM contactos_extras WHERE contactos_extras.IdCliente ='.$_POST['id'];
+	$contactos = $run->select($query);
+
+	$query = 'SELECT * FROM correo_extra WHERE correo_extra.IdUsuario ='.$_POST['id'];
+	$data = $run->select($query);
+
+	$query = 'SELECT * FROM telefono_extra WHERE telefono_extra.IdUsuario ='.$_POST['id'];
+	$data = $run->select($query);
+
 	echo json_encode($data);
 ?>
