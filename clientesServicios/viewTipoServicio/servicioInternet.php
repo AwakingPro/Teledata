f76caf1c47781515fb71c7e2da651ce307ae31df
Alvaro Rincon <arincon@outlook.com>
2017-11-10 11:50:28 -0400

    cambio en formulario internet

--- clientesServicios/viewTipoServicio/servicioInternet.php
+++ clientesServicios/viewTipoServicio/servicioInternet.php
@@ -2,35 +2,12 @@
 	<div class="col-md-12">
 		<h3>Servicio de Internet</h3>
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Estación/Nodo</label>
-		<input name="nodo" class="form-control">
-		<input name="idServicio" type="hidden">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Mac del Router</label>
-		<input name="macRouter" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Mac Antena</label>
-		<input name="macAntena" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>IP Router</label>
-		<input name="ipRouter" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>IP Antena</label>
-		<input name="ipAntena" class="form-control">
-	</div>
+
 	<div class="col-md-4 form-group" rows="5">
 		<label>Fecha Instalación</label>
 		<input name="fechaInstalacion" class="form-control date">
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Técnico Instalador</label>
-		<input name="tecnicoInstalador" class="form-control">
-	</div>
+
 	<div class="col-md-4 form-group" rows="5">
 		<label>Velocidad</label>
 		<input name="velocidad" class="form-control">
@@ -39,28 +16,18 @@
 		<label>Plan</label>
 		<input name="plan" class="form-control">
 	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Estado del Servicio</label>
-		<input name="estadoServicio" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Señal de Instalación</label>
-		<input name="señalInstalacion" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Señal Actual</label>
-		<input name="señalActual" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Dirección IP AP</label>
-		<input name="ipAn" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Coordenadas Latitud</label>
-		<input name="latitud" class="form-control">
-	</div>
-	<div class="col-md-4 form-group" rows="5">
-		<label>Coordenadas Longitud</label>
-		<input name="longitud" class="form-control">
-	</div>
+
+	<input name="nodo" type="hidden">
+	<input name="idServicio" type="hidden">
+	<input name="macRouter" type="hidden">
+	<input name="macAntena" type="hidden">
+	<input name="ipRouter" type="hidden">
+	<input name="ipAntena" type="hidden">
+	<input name="tecnicoInstalador" type="hidden">
+	<input name="estadoServicio" type="hidden">
+	<input name="señalInstalacion" type="hidden">
+	<input name="señalActual" type="hidden">
+	<input name="ipAn" type="hidden">
+	<input name="latitud" type="hidden">
+	<input name="longitud" type="hidden">
 </div>
