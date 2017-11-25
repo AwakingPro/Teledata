ddbb9e7c3ab737f910f0473055f56478af8e4d10
arincon21 <arincon@outlook.com>
2017-11-25 17:13:04 -0400

    modificacion de update cliente

--- ajax/cliente/updateCliente.php
+++ ajax/cliente/updateCliente.php
@@ -1,8 +1,8 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
 
-	$Rut = isset($_POST['Rut_update']) ? trim($_POST['Rut_update']) : "";
-	$Dv = isset($_POST['Dv_update']) ? trim($_POST['Dv_update']) : "";
+	$Rut = substr($_POST['Rut_update'], 0, strpos($_POST['Rut_update'], '-'));
+	$Dv = substr($_POST['Rut_update'], - 1);
 	$Nombre = isset($_POST['Nombre_update']) ? trim($_POST['Nombre_update']) : "";
 	$Giro = isset($_POST['Giro_update']) ? trim($_POST['Giro_update']) : "";
 	$DireccionComercial = isset($_POST['DireccionComercial_update']) ? trim($_POST['DireccionComercial_update']) : "";
@@ -15,8 +15,9 @@
 	$Comuna = isset($_POST['Comuna_update']) ? trim($_POST['Comuna_update']) : "";
 	$Ciudad = isset($_POST['Ciudad_update']) ? trim($_POST['Ciudad_update']) : "";
 	$IdCliente = isset($_POST['IdCliente']) ? trim($_POST['IdCliente']) : "";
+	$CodigoCliente = isset($_POST['Rut_update']) ? trim($_POST['Rut_update']) : "";
 
-	$query = "UPDATE personaempresa SET rut = '$Rut', dv = '$Dv', nombre = '$Nombre', giro = '$Giro', direccion = '$DireccionComercial', correo = '$Correo', contacto = '$Contacto', comentario = '$Comentario', telefono = '$Telefono', tipo_cliente = '$TipoCliente', comuna = '$Comuna', ciudad = '$Ciudad' WHERE id = '$IdCliente'";
+	$query = "UPDATE personaempresa SET CodigoCliente = '$CodigoCliente',  rut = '$Rut', dv = '$Dv', nombre = '$Nombre', giro = '$Giro', direccion = '$DireccionComercial', correo = '$Correo', contacto = '$Contacto', comentario = '$Comentario', telefono = '$Telefono', tipo_cliente = '$TipoCliente', comuna = '$Comuna', ciudad = '$Ciudad' WHERE id = '$IdCliente'";
 	$run = new Method;
 	$data = $run->update($query);
 	echo $data;
