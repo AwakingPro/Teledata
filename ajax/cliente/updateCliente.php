36714bf996e64d46782e63ae338aca75d73918f6
Alvaro Rincon <arincon@outlook.com>
2017-12-01 10:20:17 -0400

    modificacion de cliente update

--- ajax/cliente/updateCliente.php
+++ ajax/cliente/updateCliente.php
@@ -22,34 +22,35 @@
 	$data = $run->update($query);
 
 	if (count($_POST['extra_telefono']) > 0) {
-				for ($i=0; $i < count($_POST['extra_telefono']); $i++) {
-					if ($_POST['extra_telefono'][$i] != "") {
-						 $query = "INSERT INTO telefono_extra (IdUsuario, Telefono) VALUES ('".$id."', '".$_POST['extra_telefono'][$i]."')";
-						$data = $run->insert($query);
-					}
-
-				}
+		$run->delete("DELETE FROM telefono_extra WHERE IdUsuario = '$IdCliente'");
+		for ($i=0; $i < count($_POST['extra_telefono']); $i++) {
+			if ($_POST['extra_telefono'][$i] != "") {
+				 $query = "INSERT INTO telefono_extra (IdUsuario, Telefono) VALUES ('".$IdCliente."', '".$_POST['extra_telefono'][$i]."')";
+				$data = $run->insert($query);
 			}
 
-			if (count($_POST['extra_correo']) > 0) {
-				for ($i=0; $i < count($_POST['extra_correo']); $i++) {
-					if ($_POST['extra_correo'][$i] != "") {
-						 $query = "INSERT INTO correo_extra (IdUsuario, Correo) VALUES ('".$id."', '".$_POST['extra_correo'][$i]."')";
-						$data = $run->insert($query);
-					}
+		}
+	}
 
-				}
+	if (count($_POST['extra_correo']) > 0) {
+		for ($i=0; $i < count($_POST['extra_correo']); $i++) {
+			if ($_POST['extra_correo'][$i] != "") {
+				 $query = "INSERT INTO correo_extra (IdUsuario, Correo) VALUES ('".$id."', '".$_POST['extra_correo'][$i]."')";
+				$data = $run->insert($query);
 			}
 
-			if (count($_POST['extra_TipoContacto']) > 0) {
-				for ($i=0; $i < count($_POST['extra_TipoContacto']); $i++) {
-					if ($_POST['extra_TipoContacto'][$i] != "") {
-						$query = "INSERT INTO contactos_extras (IdCliente, TipoContacto, Contacto) VALUES ('".$id."', '".$_POST['extra_TipoContacto'][$i]."', '".$_POST['extra_Contacto'][$i]."');";
-						$data = $run->insert($query);
-					}
+		}
+	}
 
-				}
+	if (count($_POST['extra_TipoContacto']) > 0) {
+		for ($i=0; $i < count($_POST['extra_TipoContacto']); $i++) {
+			if ($_POST['extra_TipoContacto'][$i] != "") {
+				$query = "INSERT INTO contactos_extras (IdCliente, TipoContacto, Contacto) VALUES ('".$id."', '".$_POST['extra_TipoContacto'][$i]."', '".$_POST['extra_Contacto'][$i]."');";
+				$data = $run->insert($query);
 			}
+
+		}
+	}
 
 	echo $data;
  ?>
