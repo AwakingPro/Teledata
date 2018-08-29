58129634310b9663457deb74445c5c306eb5e9c8
Oswaldo <bfsraptor@gmail.com>
2018-08-29 17:54:13 -0300

    asd

--- includes/facturacion/facturas/generarFacturas.php
+++ includes/facturacion/facturas/generarFacturas.php
@@ -1,6 +1,6 @@
 <?php 
 
-	include("../../../class/facturacion/facturas/FacturaClass.php");
+	include("/var/www/html/Teledata/class/facturacion/facturas/FacturaClass.php");
 
 	$Factura = new Factura();
 	$Factura->generarFacturas();
