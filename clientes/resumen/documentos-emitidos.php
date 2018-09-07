95a40b49a4b82b9058855cf2906d0eb001da5351
DanielGeek <daniel30081990@gmail.com>
2018-09-07 17:41:15 -0400

    agregadas tablas
    agregadas tablas de documentos emitidos y pagados

--- clientes/resumen/documentos-emitidos.php
+++ clientes/resumen/documentos-emitidos.php
@@ -2,7 +2,28 @@
     <div class="panel">
         <div class="panel-body">
             <h3 style="margin-top: 0">Últimos Documentos Emitidos</h3>
-            <div class="listaCliente"></div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                            <table id="TableDocEmitidos" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">N* de Documento</th>
+                                        <th class="text-center">Tipo de Documento</th>
+                                        <th class="text-center">Fecha Emisión</th>
+                                        <th class="text-center">Fecha Vencimiento</th>
+                                        <th class="text-center">Total Doc.</th>
+                                        <th class="text-center">Saldo Doc.</th>
+                                        <th class="text-center">Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+            <input type="hidden" name="rutCliente" value="<?php echo $Rut; ?>">
         </div>
     </div>
 </div>
--- clientes/resumenCliente.php
+++ clientes/resumenCliente.php
@@ -86,6 +86,7 @@ $id_cliente = $_GET['cliente'];
 	<script src="../js/methods_global/methods.js"></script>
 	<script src="../plugins/datatables/media/js/jquery.dataTables.js"></script>
 	<script src="../plugins/datatables/media/js/dataTables.bootstrap.js"></script>
-	<script src="../js/bienvenida/controller.js"></script>
+	<!-- <script src="../js/bienvenida/controller.js"></script> -->
+	<script src="../js/resumenCliente/resumenCliente.js"></script>
 </body>
 </html>
