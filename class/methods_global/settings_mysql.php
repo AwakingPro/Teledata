f8d42b399f09663f8366844d796005a01c4f4122
DanielGeek <daniel30081990@gmail.com>
2018-10-23 18:45:20 -0300

    config

--- class/methods_global/settings_mysql.php
+++ class/methods_global/settings_mysql.php
@@ -3,8 +3,8 @@
 	* Datos para conexion al servidor de bd mysql
 	*/
 	date_default_timezone_set('Chile/Continental');
-	// define("host","localhost");
-	define("host","131.0.108.31");
+	define("host","localhost");
+	// define("host","131.0.108.31");
 	define("user","root");
 	// define("password","");
 	define("password","ks1qnl.,2236");
