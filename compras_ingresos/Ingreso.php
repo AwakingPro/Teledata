65d3758cfd08d4fb19e3ccd098f9f8866905d8d9
DanielGeek <daniel30081990@gmail.com>
2018-10-29 16:21:44 -0300

    head factura proveedor

--- compras_ingresos/Ingreso.php
+++ compras_ingresos/Ingreso.php
@@ -35,7 +35,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title">Facturas de Proveedores <button type="button" data-dismiss="modal" class="close f-25" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button></h4>
+                        <h4 class="modal-title">Agregar Factura de Proveedor <button type="button" data-dismiss="modal" class="close f-25" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button></h4>
                     </div>
                     <div class="modal-body">
                         <div class="row" style="padding:20px">
