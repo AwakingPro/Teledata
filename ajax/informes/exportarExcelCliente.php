809e9cc2b11ac9a2afa7f6a30061029269c231ca
DanielGeek <daniel30081990@gmail.com>
2018-11-21 19:02:45 -0300

    asasas

--- ajax/informes/exportarExcelCliente.php
+++ ajax/informes/exportarExcelCliente.php
@@ -70,8 +70,10 @@ if (count($data) > 0) {
 	// print_r($data[10]); exit;
 	$index = 2;
 	for ($i=0; $i < count($data) ; $i++) {
-		if(!isset($data[$i][5]))
+		if(isset($data[$i][5]))
 			$FechaInstalacion = \DateTime::createFromFormat('Y-m-d', $data[$i][5])->format('d-m-Y');
+		else
+			$FechaInstalacion = 'Sin Fecha';
 		
 		// else{
 		// 	$data[$i][5] = 'Sin Fecha';
