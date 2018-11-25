96ce422b74c232d0e9528666e41bc5d92c215cac
DanielGeek <daniel30081990@gmail.com>
2018-11-25 16:38:24 -0300

    asas

--- pruebas/index.php
+++ pruebas/index.php
@@ -2,9 +2,9 @@
 
 $ruta = "/var/www/html/Teledata/facturacion/facturas/3072.pdf";
 $tamano = filesize($ruta);
-if( $ruta)
+if( $tamano > 0)
 	echo 'Existe su tamano es:'.$tamano;
 else
-    echo 'No Existe la ruta'.$ruta;
+    echo 'Su tamano es '.$tamano;
 
 ?>
