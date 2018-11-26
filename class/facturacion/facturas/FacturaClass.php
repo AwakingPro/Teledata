0d6b7543f9133758535f3c3215cf2a69e76477ca
DanielGeek <daniel30081990@gmail.com>
2018-11-26 10:06:57 -0300

    asass

--- class/facturacion/facturas/FacturaClass.php
+++ class/facturacion/facturas/FacturaClass.php
@@ -3045,7 +3045,8 @@
                     $TipoDocumento = 'Factura';
                 }
                 $Asunto = $TipoDocumento . ' #' . $NumeroDocumento . ' Teledata';
-
+                $espacios2 = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+                $espacios = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
                 $Html =
                 "<html>
                     <head>
@@ -3060,6 +3061,13 @@
                     <body>
                     ESTIMADO(A) ".$Nombre.",<br>
                         La ".$TipoDocumento." #".$NumeroDocumento." se genero con exito y ha sido adjuntada en este correo.<br><br>
+                        <b>Para transferencia o depósitos, los datos de nuestra cuenta son:</b><br><br>
+                        RAZÓN SOCIAL:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>TELEDATA CHILE SPA.</b><br>
+                        RUT:".$espacios."<b>76.722.248-3</b><br>
+                        BANCO:".$espacios2."<b>BANCO DE CHILE</b><br>
+                        TIPO DE CUENTA:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>CUENTA CORRIENTE</b><br>
+                        NUMERO DE CUENTA:&nbsp;<b>268-04500-03</b><br>
+                        CORREO:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><a href='mailto:pagos@teledata.cl'>pagos@teledata.cl</a></b><br><br>
                         Saludos.
                     </body>
                 </html>";
