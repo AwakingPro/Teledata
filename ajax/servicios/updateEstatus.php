1e60c893fae27151c90051d76406efde570a1890
DanielGeek <daniel30081990@gmail.com>
2019-01-09 12:41:39 -0300

    actualizar servicio correctamente

--- ajax/servicios/updateEstatus.php
+++ ajax/servicios/updateEstatus.php
@@ -3,9 +3,10 @@
 	
     $run = new Method;
     $Id = isset($_POST['Id']) ? trim($_POST['Id']) : "";
-	$Activo = isset($_POST['Activo']) ? trim($_POST['Activo']) : "";
+    $Activo = isset($_POST['Activo']) ? trim($_POST['Activo']) : "";
     $FechaInicioDesactivacion = isset($_POST['FechaInicioDesactivacion']) ? trim($_POST['FechaInicioDesactivacion']) : "";
     $FechaFinalDesactivacion = isset($_POST['FechaFinalDesactivacion']) ? trim($_POST['FechaFinalDesactivacion']) : "";
+    //inactivo 0, suspendido 2, activo 1
     if(!$Activo || $Activo == 2){
         if($FechaInicioDesactivacion && $FechaFinalDesactivacion){
             $FechaInicioDesactivacion = DateTime::createFromFormat('Y/m/d', $FechaInicioDesactivacion);
@@ -27,7 +28,9 @@
         $FechaFinalDesactivacion = 'NULL';
     }
 
-	$query = "UPDATE servicios SET FechaInicioDesactivacion = $FechaInicioDesactivacion, FechaFinalDesactivacion = $FechaFinalDesactivacion WHERE Id = '".$Id."'";
+	$query = "UPDATE servicios SET FechaInicioDesactivacion = $FechaInicioDesactivacion, 
+              FechaFinalDesactivacion = $FechaFinalDesactivacion, EstatusServicio = $Activo
+              WHERE Id = '".$Id."'";
 	$update = $run->update($query);
 
 	echo 1;
