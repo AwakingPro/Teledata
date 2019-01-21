40ebf3b92bb5520e3c7383d057d1bcfbe11f92b4
DanielGeek <daniel30081990@gmail.com>
2019-01-21 16:26:20 -0300

    asasasas

--- class/methods_global/settings_mysql.php
+++ class/methods_global/settings_mysql.php
@@ -4,9 +4,7 @@
 	*/
 	date_default_timezone_set('Chile/Continental');
 	define("host","localhost");
-	// define("host","131.0.108.31");
 	define("user","root");
-	// define("password","");
 	define("password","ks1qnl.,2236");
 	define("nameDataBase","teledata");
  ?>
