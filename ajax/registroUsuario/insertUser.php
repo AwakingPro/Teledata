349d88db7562d40b34fc2251237541aad381c043
DanielGeek <daniel30081990@gmail.com>
2019-02-01 17:21:17 -0300

    agregado nuevo usuario para prueba tarea

--- ajax/registroUsuario/insertUser.php
+++ ajax/registroUsuario/insertUser.php
@@ -4,9 +4,10 @@
 	$usuario = isset($_POST['usuario']) ? trim($_POST['usuario']) : "";
 	$nombre = isset($_POST['nombre']) ? trim($_POST['nombre']) : "";
 	$correo = isset($_POST['correo']) ? trim($_POST['correo']) : "";
-	
+	$privilegio = $_POST['nivel'];
+
 	$query = " INSERT INTO usuarios (usuario, nombre, clave, nivel, cargo, email, sexo, tipo_usuario) 
-	VALUES ('".$usuario."', '".$nombre."', '".$clave."', '".$_POST['nivel']."', '".$_POST['cargo']."', '".$correo."', '', '0')";
+	VALUES ('".$usuario."', '".$nombre."', '".$clave."', '".$privilegio."', '".$_POST['cargo']."', '".$correo."', '', '".$privilegio."')";
 	$run = new Method;
 	$data = $run->insert($query);
 	echo $data;
