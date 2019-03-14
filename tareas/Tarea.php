0a7751b821da4bd7bc919f6fb1aba9b872ce3e3d
DanielGeek <daniel30081990@gmail.com>
2019-03-14 17:38:18 -0300

    agregado modal con info del cliente de un servicio

--- tareas/Tarea.php
+++ tareas/Tarea.php
@@ -101,18 +101,7 @@
 
     // Muestra Modal para Editar Servicio
     include '../componentes/componentes_servicios/modal_EditarServicio.php';
+
+    // Muestra Modal con info del cliente
+    include '../componentes/componentes_servicios/modal_InfoCliente.php';
 ?>
-
-<div class="modal fade" role="dialog" id="InfoClienteTable">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Datos del cliente</h4>
-            </div>
-            <div class="modal-body container-dataCliente">
-
-            </div>
-        </div>
-    </div>
-</div>
