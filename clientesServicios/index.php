026aa5b0a1648b5591688298339e58812ff8077e
DanielGeek <daniel30081990@gmail.com>
2019-03-22 14:35:54 -0300

    corregido ver fechas de servicios, eliminado codigo redundante

--- clientesServicios/index.php
+++ clientesServicios/index.php
@@ -166,37 +166,6 @@
 					<?php
 					include '../componentes/componentes_servicios/form_activa_servicios.php';
 					?>
-						<!-- <form id = "formEstatus">
-							<input type="hidden" class="Id" name="Id" id="Id">							
-							<div class="row" style="padding:20px">
-								<div class="col-md-12">
-									<div class="form-group">
-										<label class="compo-grupo">Estado del Servicio</label>
-										<div class="compo-grupo">
-											<select id="Activo" name="Activo" class="form-control selectpicker" data-live-search="true">
-												<option value="">Seleccione...</option>
-												<option value="1">Activo</option>
-												<option value="2">Suspendido</option>
-												<option value="0">Cortado</option>
-											</select>
-										</div>
-										<br>
-										<div id="divFechaActivacion" style="display:none">
-											<label>Fecha de Activación</label>
-											<div class="form-group">
-												<div id="date-range">
-													<div class="input-daterange input-group" id="datepicker">
-														<input type="text" class="form-control" id="FechaInicioDesactivacion" name="FechaInicioDesactivacion" data-nombre="Fecha de Activación" />
-														<span class="input-group-addon">a</span>
-														<input type="text" class="form-control" id="FechaFinalDesactivacion" name="FechaFinalDesactivacion" data-nombre="Fecha de Activación" />
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</form> -->
 					</div><!-- /.modal-body -->
 					<div class="modal-footer p-b-20 m-b-20">
 						<div class="col-sm-12">
