f33eb5ffa512291b429a0b94ce59572e60aa7507
DanielGeek <daniel30081990@gmail.com>
2019-04-25 16:35:45 -0400

    inhabilitar botones

--- js/facturacion/Facturacion.js
+++ js/facturacion/Facturacion.js
@@ -184,7 +184,14 @@ $(document).ready(function() {
                                 if(row.PermitirFactura == 1){
                                     //temporal
                                     //comentar el prop y sustituir el not-allowed por pointer de Facturar luego de que se puedan enviar correos 
+                                    $('.VisualizarLote').prop('disabled', true);
                                     $('.Facturar').prop('disabled', true);
+                                    $('.fa-list-alt').prop('disabled', true);
+                                    $('.Prefacturacion').prop('disabled', true);
+                                    $('.Prefactura').prop('disabled', true);
+                                    $('.Eliminar').prop('disabled', true);
+                                    $('.Referencia').prop('disabled', true);
+
                                     Icono += '<i title="Facturar" style="cursor: pointer; margin: 0 10px; font-size:15px;" class="fa fa-money Facturar" data-trigger="hover" data-toggle="popover" data-placement="top" data-content="Facturar hacia la API de Bsale" title="" data-container="body"></i>'
                                 }
                                 if(IdUsuarioSession == 109 || IdUsuarioSession == 116 || IdUsuarioSession == 104){
