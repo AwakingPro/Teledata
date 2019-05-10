e79943e9908f1b183ea3c3d5dd12916049d18fcf
DanielGeek <daniel30081990@gmail.com>
2019-05-10 12:48:25 -0400

    Agregado nuevo metodo para comprovar documentos de clientes

---
+++ includes/facturacion/facturas/verificarDocumentosEmitidos.php
@@ -0,0 +1,11 @@
+<?php 
+
+	// include("/var/www/html/Teledata/class/facturacion/facturas/FacturaClass.php");
+	require("../../../plugins/PHPMailer-master/class.phpmailer.php");
+    require("../../../plugins/PHPMailer-master/class.smtp.php");
+    include('../../../class/email/EmailClass.php');
+	include("../../../class/facturacion/facturas/FacturaClass.php");
+	$Factura = new Factura();
+	$Factura->verificarDocumentosEmitidos();
+	
+?>
