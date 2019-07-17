1ebcdb11a00431842aa3b8204085c7dcb93957c4
DanielGeek <daniel30081990@gmail.com>
2019-07-17 16:32:55 -0400

    query correcta

--- includes/facturacion/facturas/getServiciosFacturados.php
+++ includes/facturacion/facturas/getServiciosFacturados.php
@@ -1,11 +1,10 @@
 <?php 
 
-	// include("../../../class/facturacion/facturas/FacturaClass.php");
-	include("/var/www/html/Teledata/class/facturacion/facturas/FacturaClass.php");
+	include("../../../class/facturacion/facturas/FacturaClass.php");
 	$Factura = new Factura();
 	
-	$ToReturn = $Factura->getServiciosFacturados($_POST['RUT']);
-	echo $ToReturn;
+	$Factura->getServiciosFacturados($_POST['RUT']);
+	
 	
 	
 ?>
