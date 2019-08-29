7e4daa1ad8b46f3efe6eeccc23c0e225969b3d66
DanielGeek <daniel30081990@gmail.com>
2019-08-29 17:25:49 -0400

    Agregado ver marca al agregar modelo producto y loader al eliminar registrros

--- modelo_producto/ModeloProducto.php
+++ modelo_producto/ModeloProducto.php
@@ -142,7 +142,7 @@
                                                                                             <th class="text-center">Acción</th>
                                                                                         </tr>
                                                                                     </thead>
-                                                                                    <tbody>
+                                                                                    <tbody class="TableLoader">
                                                                                     </tbody>
                                                                                 </table>
                                                                             </div>
