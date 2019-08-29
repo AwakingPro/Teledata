c8199a6f1bf8f3e9b15568bec99173f69e4e42a1
DanielGeek <daniel30081990@gmail.com>
2019-08-29 12:26:10 -0400

    Comprobar si esta logeado el usuario y cambiar estado

--- ajax/login/session.php
+++ ajax/login/session.php
@@ -1,15 +1,21 @@
 <?php
 	require_once('../../class/methods_global/methods.php');
 	$run = new Method;
-	$query = 'SELECT clave, id, nivel FROM usuarios WHERE usuario="'.$_POST['usuario'].'"';
+	$query = 'SELECT clave, id, nivel, estaLogin FROM usuarios WHERE usuario="'.$_POST['usuario'].'"';
 	$data = $run->select($query);
 	if (count($data) > 0) {
 		if (password_verify($_POST['password'], $data[0]['clave'])) {
-			
-			$_SESSION['idUsuario'] = $data[0]['id'];
-			$_SESSION['idNivel'] = $data[0]['nivel'];
-			$run->insert('INSERT INTO log_login (IdUsuario, Usuario, Fecha, Proceso) VALUES ("'.$data[0]['id'].'", "'.$_POST['usuario'].'", "'.date("Y-m-d H:i:s").'", "CORRECTO");');
-			$array = array(true,'bienvenida/bienvenida.php');
+			if($data[0]['estaLogin'] == 1){
+				$run->insert('INSERT INTO log_login (IdUsuario, Usuario, Fecha, Proceso) VALUES ("'.$data[0]['id'].'", "'.$_POST['usuario'].'", "'.date("Y-m-d H:i:s").'", "OTRO USUARIO INTENTO ENTRAR DESDE LA IP: '.$_SERVER['REMOTE_ADDR'].' ");');
+				$array = array(2,null);
+			}else{
+				$run->update("UPDATE usuarios set estaLogin = 1 WHERE id = '".$data[0]['id']."' ");
+				$_SESSION['idUsuario'] = $data[0]['id'];
+				$_SESSION['idNivel'] = $data[0]['nivel'];
+				$run->insert('INSERT INTO log_login (IdUsuario, Usuario, Fecha, Proceso) VALUES ("'.$data[0]['id'].'", "'.$_POST['usuario'].'", "'.date("Y-m-d H:i:s").'", "CORRECTO");');
+				$array = array(true,'bienvenida/bienvenida.php');
+			}
+
 		}else{
 			$run->insert('INSERT INTO log_login (IdUsuario, Usuario, Fecha, Proceso) VALUES (0, "'.$_POST['usuario'].'", "'.date("Y-m-d H:i:s").'", "INCORRECTO");');
 			$array = array(false,null);
--- destruir_sesion.php
+++ destruir_sesion.php
@@ -1,5 +1,9 @@
 <?php
+    require_once('class/methods_global/methods.php');
+    
     session_start();
+    $run = new Method;
+    $run->update("UPDATE usuarios set estaLogin = 0 WHERE id = '".$_SESSION['idUsuario']."' ");
     session_destroy();
     header('Location: index.php');
 ?>
