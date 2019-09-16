16f57b95e856ae3cb44bbf21bcb1f397ff464a34
DanielGeek <daniel30081990@gmail.com>
2019-09-16 11:14:19 -0300

    asasas

--- class/facturacion/facturas/FacturaClass.php
+++ class/facturacion/facturas/FacturaClass.php
@@ -4385,7 +4385,7 @@
             $variables_globales = $run->select($query);
             $access_token = $variables_globales[0]['access_token'];
             //DOCUMENTOS
-            $limitDocumentos = 1000;
+            $limitDocumentos = 900;
             $url='https://api.bsale.cl/v1/documents.json?expand=[references,client,details]&limit='.$limitDocumentos;
             // Inicia cURL
             $session = curl_init($url);
