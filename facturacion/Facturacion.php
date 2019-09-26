228d409da9e080d584eb36c17c0b8ac5755d3482
DanielGeek <daniel30081990@gmail.com>
2019-09-26 15:41:12 -0300

    Agregados nuevos campos en fac por lotes

--- facturacion/Facturacion.php
+++ facturacion/Facturacion.php
@@ -160,6 +160,8 @@
                                                         <th class="text-center">Cliente</th>
                                                         <th class="text-center">RUT</th>
                                                         <th class="text-center">Grupo Factura</th>
+                                                        <th class="text-center">Neto</th>
+                                                        <th class="text-center">I.V.A</th>
                                                         <th class="text-center">Total</th>
                                                         <th class="text-center">Acción</th>
                                                     </tr>
