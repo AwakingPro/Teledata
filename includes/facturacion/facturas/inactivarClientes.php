f161e7e66e906a3168a944987d82b45137777590
teledatageek <dangel@teledata.cl>
2019-10-14 10:28:32 -0300

    inactivar clientes por termino de contrato

---
+++ includes/facturacion/facturas/inactivarClientes.php
@@ -0,0 +1,7 @@
+<?php
+
+ include("/var/www/html/Teledata/class/facturacion/facturas/FacturaClass.php");
+//include("../../../class/facturacion/facturas/FacturaClass.php");
+$Factura = new Factura();
+echo $Factura->inactivarClientes();
+?>
